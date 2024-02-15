3220375a7093fd4d5ffd897d93761b0af0f2470f
anggimude <hyuk.jang@mail.utoronto.ca>
2024-02-14 23:27:44 -0500

    push

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,6 +10,16 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+# Checks if male average start month has the class "numeric"
+mavg_start_month |> class() == "numeric"
+# Checks if birth year in clean female data has the class "numeric"
+clean_female_data$birth_year....afemale_birth_year |> class() == "numeric"
+# Checks if award performance in clean white data is bigger or equal to "0"
+clean_white_data$award_performance....awhite_awd_perf >= 0
+# Checks if civilian complaints of clean black data is bigger or equal to "0"
+clean_black_data$complaints....ablack_complaints >= 0
+# Checks if observations of clean male data has the class "integer"
+clean_male_data$observations....male_observation |> class() == "integer"
